be2ddfadb33c03318d7a9dbf2c3ffa829d872d44
JensenL <jensen2320@gmail.com>
2025-09-30 05:18:45 +0800

    Fix Vercel API configuration and remove server.js conflicts

--- api/calendar.js
+++ api/calendar.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 // Google Calendar API handler for Vercel
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
